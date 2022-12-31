a46e6d336f7b729a0c6224175794f0d861d12604
Lauren Tyson <lauren.wlkr@gmail.com>
2022-12-31 09:25:21 -0700

    Fix syntax error and add secure password and validation

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,9 @@
 class User < ApplicationRecord
     has_many :reviews
-    has_many :dog_walkers, :reviews
+    has_many :dog_walkers, through: :reviews
+
+    has_secure_password
+  
+    validates :username, presence: true, uniqueness: true
+    
 end
