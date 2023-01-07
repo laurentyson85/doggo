0823bf48b2e6b045f849051796fdd062498848c7
Lauren Tyson <lauren.wlkr@gmail.com>
2023-01-07 13:32:29 -0700

    Add walker details

--- client/src/components/WalkerDetails.js
+++ client/src/components/WalkerDetails.js
@@ -13,15 +13,19 @@ function WalkerDetails({}) {
         })
       }, [])
       
-      console.log(walker)
+      console.log(walker) //remove this console.log later
+
+      //need to display dog walker and reviews. If no reviews display language to click button to add
+      //add button and simple form to create a review
+      //make post request
+      //after post, think about how I will update this state to show newly created review
 
     return (
         <div className="walkerDetails">                   
           <img src={walker.image_url} alt="image_url" />
-          {/* <p><span style={{fontWeight: "bold"}}>Company slogan</span> {company_slogan}</p>
-          <p>{company_name} is hiring a {title}. This {position} is skilled in: {key_skill}</p>
-          <p><span style={{fontWeight: "bold"}}>Employment Type</span> {employment}</p>                           
-          {expired ?
+          <p><span style={{fontWeight: "bold"}}>Name: </span> {walker.name}</p>
+          <p>I love all dogs, but my personal favorite is a {walker.favorite_dog_breed}. Contact me at {walker.phone} to schedule a walk!</p>
+          {/* {expired ?
             <>
               <p>The position is expired.</p>
               <button className="expired" onClick={handleDeleteClick}>No longer on the market? Remove it</button>
