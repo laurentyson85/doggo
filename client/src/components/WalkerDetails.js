3fe8041b1cb562559faa409b100ff982306c5525
Lauren Tyson <lauren.wlkr@gmail.com>
2023-01-07 12:41:04 -0700

    Add details to walker show page

--- client/src/components/WalkerDetails.js
+++ client/src/components/WalkerDetails.js
@@ -1,12 +1,38 @@
-import React from "react";
-import { useParams, useNavigate} from "react-router-dom"
+import React, { useState, useEffect } from 'react';
+import { useParams } from "react-router-dom"
 
 function WalkerDetails({}) {
+    const [walker, setWalker] = useState([])
+    const { id } = useParams()
+
+    useEffect(() => {
+        fetch(`/dog_walkers/${id}`)
+        .then(response => response.json())
+        .then(data => {
+            setWalker(data)
+        })
+      }, [])
+      
+      console.log(walker)
 
     return (
-        <div className="walkerDetails">        
-        Walker Details
-      </div>      
+        <div className="walkerDetails">                   
+          <img src={walker.image_url} alt="image_url" />
+          {/* <p><span style={{fontWeight: "bold"}}>Company slogan</span> {company_slogan}</p>
+          <p>{company_name} is hiring a {title}. This {position} is skilled in: {key_skill}</p>
+          <p><span style={{fontWeight: "bold"}}>Employment Type</span> {employment}</p>                           
+          {expired ?
+            <>
+              <p>The position is expired.</p>
+              <button className="expired" onClick={handleDeleteClick}>No longer on the market? Remove it</button>
+            </>    
+          :
+            <>
+              <p>Active job posting</p>
+              <button className="open job" onClick={handleHireClick}>Hire a Hipster!</button>
+            </>                
+          } */}
+       </div>      
     )
   }
   
